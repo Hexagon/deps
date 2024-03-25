a1f23f76ac5f5ef306e3fd628fea08b155e47533
Hexagon <hexagon@56k.guru>
2024-03-25 21:12:48 +0100

    Fix entrypoint

--- deps.ts
+++ deps.ts
@@ -12,55 +12,66 @@ import { DenoLock, getDenoLockPath } from "./src/lockfile.ts";
 import { analyzeDependencies } from "./src/analyzer.ts";
 import { Package } from "./src/package.ts";
 
-// Parse arguments
-const parsedArgs = parseArgs();
+async function main() {
+  // Parse arguments
+  const parsedArgs = parseArgs();
 
-// Handle help argument
-if (parsedArgs.count("help")) {
-  printHelpAndExit();
-}
-
-// Entry point
-let packages: Package[] | null = null;
-let lockFile: DenoLock | null = null;
-
-const targetPath = parsedArgs.get("target");
-if (targetPath?.toString() === "true") {
-  printErrorAndExit(new Error("--target passed without a path"));
-}
-
-// Read lockfile
-try {
-  lockFile = await tryReadJsoncFile(
-    getDenoLockPath(targetPath as string | undefined || ""),
-  );
-} catch (_e) {
-  // Ignore, lock file really not needed
-}
-
-// Analyze the dependencies, exit on error
-try {
-  packages = await analyzeDependencies(
-    lockFile,
-    targetPath as string | undefined || "",
-  );
-} catch (e) {
-  printErrorAndExit(e);
-}
-
-// Automatically set ignore
-const allowUnused = parsedArgs.count("allow-unused") > 0 || !lockFile;
-
-// If packages were found
-if (packages) {
-  // If not silent, print table
-  if (!parsedArgs.count("slim") && packages) {
-    printTable(packages, allowUnused);
+  // Handle help argument
+  if (parsedArgs.count("help")) {
+    printHelpAndExit();
   }
 
-  printStatsAndExit(stats(packages), !!parsedArgs.count("slim"), allowUnused);
+  // Entry point
+  let packages: Package[] | null = null;
+  let lockFile: DenoLock | null = null;
 
-  // If no packages were found
-} else {
-  printSuccessAndExit("No dependencies found.");
+  const targetPath = parsedArgs.get("target");
+  if (targetPath?.toString() === "true") {
+    printErrorAndExit(new Error("--target passed without a path"));
+  }
+
+  // Read lockfile
+  try {
+    lockFile = await tryReadJsoncFile(
+      getDenoLockPath(targetPath as string | undefined || ""),
+    );
+  } catch (_e) {
+    // Ignore, lock file really not needed
+  }
+
+  // Analyze the dependencies, exit on error
+  try {
+    packages = await analyzeDependencies(
+      lockFile,
+      targetPath as string | undefined || "",
+    );
+  } catch (e) {
+    printErrorAndExit(e);
+  }
+
+  // Automatically set ignore
+  const allowUnused = parsedArgs.count("allow-unused") > 0 || !lockFile;
+
+  // If packages were found
+  if (packages) {
+    // If not silent, print table
+    if (!parsedArgs.count("slim") && packages) {
+      printTable(packages, allowUnused);
+    }
+
+    printStatsAndExit(stats(packages), !!parsedArgs.count("slim"), allowUnused);
+
+    // If no packages were found
+  } else {
+    printSuccessAndExit("No dependencies found.");
+  }
 }
+
+if (import.meta.main) main();
+
+export * from "./src/configfile.ts";
+export * from "./src/jsr.ts";
+export * from "./src/npm.ts";
+export * from "./src/lockfile.ts";
+export * from "./src/package.ts";
+export * from "./src/utils.ts";
